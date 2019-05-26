79d395db36c73619981e7d32c86709bd76303d8d
JoaoCosme <cosmejoaopedro@gmail.com>
2019-05-26 19:37:21 -0300

    Atualizaçao
    Atualizacao com matrizes e criando arquivo novo (comentado)

--- Bomberman.c
+++ Bomberman.c
@@ -2,50 +2,83 @@
 #include<stdlib.h>
 #include<conio.h>
 #include<string.h>
-
+#include<windows.h>
 
 typedef struct pos_st
 {
     int x,y;
 } pos;
 
-void imprime(char f[]);
+void imprime(char f[100][100]);
+void inicializa(char f[],char atual[100][100]);
 pos encontra (char tipo,char f[]);
 int main()
 {
     pos jogador;
-    char c,f[100];
+    char c,f[100],atual[100][100];
     printf("Entre o nome do arquivo que deseja abrir(sem o .txt no final):");
     gets(f); //fazer sem gets depois
+    //strcpy(atual,"Atual.txt");
     strcat(f,".txt");
     printf("Entre o caractere que deseja encontrar: ");
     scanf("%c",&c);
-    imprime(f);
-    jogador=encontra(c,f);
+    inicializa(f,atual);
+    jogador=encontra(c,atual);
     printf("\nO jogador se encontra em %d x e %d y\n",jogador.x,jogador.y);
+
+     while(1)
+     {
+         imprime(atual);
+         //Sleep(100); Só serve para o windows tambem
+         system("cls"); //Só serve para o windows, depois podemos trocar por outra função
+     }
+
     return 0;
 }
 
-void imprime(char f[])
+void inicializa(char f[],char atual[100][100])
 {
-    FILE *mapa;
+    int i=0;
+    FILE *mapa;//*atual;
     char texto[100];
     mapa=fopen(f,"r");
+    //atual=fopen("Atual.txt","w+");
     while (fgets(texto,100,mapa)!=NULL)
+        {
+        strcpy(atual[i],texto);
+        i++;
+        }
+        //fputs(texto,atual);
     fclose(mapa);
+    //fclose(atual);
 }
 
-pos encontra (char tipo,char f[]){
+void imprime(char f[100][100])
+{
+    /*
+    FILE *atual;
+    char texto[100];
+    atual=fopen("Atual.txt","r+");
+    while (fgets(texto,100,atual)!=NULL)
+        printf("%s",texto);
+    fclose(atual);
+    */
+    int i=0;
+    for(i=0;i<50;i++)
+        printf("%s",f[i]);
+}
+
+pos encontra (char tipo,char f[])
+{
     pos caractere;
     caractere.y=0; //Por algum motivo se eu nao declaro o .y como 0 aqui da erro
     int i=0,xt,colunas=0,enc=0;
     FILE *mapa;
-    char texto[100]={ };
+    char texto[100]= { };
     mapa=fopen(f,"r");
     while (fgets(texto,100,mapa)!=NULL) //Essa função trata linha do arquivo como uma string, entao tanto para imprimir para quanto buscar tem que ter essa mentalidade
     {
         xt=strlen(texto);
-        printf("%s",texto);
         colunas++;
         if(!enc)
         {
