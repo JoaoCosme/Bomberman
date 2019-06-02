b065f7dfdea079da8d2b146679623f9488fc9394
JoaoCosme <cosmejoaopedro@gmail.com>
2019-06-02 12:11:06 -0300

    Colisão
    -Adicionei impedimento de movimento se a proxima posição for diferente de um espaço em branco
    -Agora depois de mover o jogador a matriz com o mapa é atualizada para bater com a tela

--- Bomberman.c
+++ Bomberman.c
@@ -5,27 +5,30 @@
 #include <windows.h>
 //#define _NOCURSOR
 
-typedef struct {
+typedef struct
+{
     int x, y;
-}tipo_pos;
+} tipo_pos;
 
-typedef struct {
+typedef struct
+{
     char tamanho[25][62];
     int largura, altura;
-}tipo_mapa;
+} tipo_mapa;
 
 
 void imprime(tipo_mapa mapa);
 void inicializa(char nome_arquivo[], tipo_mapa *mapa);
 tipo_pos encontra (char personagem, tipo_mapa mapa);
-//void atualiza(tipo_mapa mapa, tipo_pos jogador);
-void movimenta(tipo_pos *jogador, int direcao, char c, tipo_mapa mapa);
+void movimenta(tipo_pos *jogador, int direcao, char c, tipo_mapa* mapa);
 char teclado();
 int menu(int inicio, tipo_mapa *mapa, tipo_pos *jogador);
 void apaga_linha();
+void atualizamapa(char c,tipo_mapa* mapa,tipo_pos* jogador,char sentido);
 
 
-int main(){
+int main()
+{
     int direcao, endgame = 0;
     tipo_pos jogador;
     char nome_arquivo[30] = "mapabeta.txt", c = '&';
@@ -46,12 +49,16 @@ int main(){
     //printf("\nO jogador se encontra em %d x e %d y\n", jogador.x, jogador.y);
     //gotoxy(jogador.x+1,jogador.y+1);
 
-    while(!endgame){
+    while(!endgame)
+    {
         direcao = getch();
-        if(direcao != 27){
-            movimenta(&jogador, direcao, c, mapa);
+        if(direcao != 27)
+        {
+            movimenta(&jogador, direcao, c, &mapa);
 
-        }else{
+        }
+        else
+        {
             endgame = menu(0, &mapa, &jogador);
 
         }
@@ -59,10 +66,12 @@ int main(){
     return 0;
 }
 
-int menu(int inicio, tipo_mapa *mapa, tipo_pos *jogador){//RETORNA 1 SE JOGO DEVE ACABAR
+int menu(int inicio, tipo_mapa *mapa, tipo_pos *jogador) //RETORNA 1 SE JOGO DEVE ACABAR
+{
     char c;
     int endgame = 0;
-    if(inicio){
+    if(inicio)
+    {
         textcolor(LIGHTRED);
         printf  ("############################################################");
         printf("\n#                                                          #");
@@ -90,57 +99,61 @@ int menu(int inicio, tipo_mapa *mapa, tipo_pos *jogador){//RETORNA 1 SE JOGO DEV
         printf("\n#                  Press any key to continue               #");
         printf("\n############################################################");
         textcolor(WHITE);
-        while(!kbhit()){}
+        while(!kbhit()) {}
         clrscr();
 
-    }else{
-        start:
+    }
+    else
+    {
+start:
         gotoxy(1,26);
         textcolor(LIGHTGREEN);
         printf("(N)New Game  (L)Load Game  (S)Save  (Q)Quit Game   (R)Return");
         textcolor(WHITE);
 
-        while(!kbhit()){}
+        while(!kbhit()) {}
         c = teclado();
-        switch(c){
-            case 'n':
-                clrscr();
-                inicializa("mapabeta.txt", mapa);
-                imprime(*mapa);
-                *jogador = encontra('&', *mapa);
-                //printf("\r\t\t\t\t\tNOVO JOGO\t\t\t\t\t");
-                break;
+        switch(c)
+        {
+        case 'n':
+            clrscr();
+            inicializa("mapabeta.txt", mapa);
+            imprime(*mapa);
+            *jogador = encontra('&', *mapa);
+            //printf("\r\t\t\t\t\tNOVO JOGO\t\t\t\t\t");
+            break;
 
-            case 'l':
-                //printf("\r\t\t\t\t\tCARREGAR JOGO\t\t\t\t\t");
-                //CHAMAR LEITURA DE MAPA JA SALVO
-                clrscr();
-                inicializa("mapajogo.txt", mapa);
-                imprime(*mapa);
-                *jogador = encontra('&', *mapa);
-                break;
+        case 'l':
+            //printf("\r\t\t\t\t\tCARREGAR JOGO\t\t\t\t\t");
+            //CHAMAR LEITURA DE MAPA JA SALVO
+            clrscr();
+            inicializa("mapajogo.txt", mapa);
+            imprime(*mapa);
+            *jogador = encontra('&', *mapa);
+            break;
 
-            case 's':
-                printf("\r\t\t\t\t\tSALVAR JOGO\t\t\t\t\t");
-                //ESCREVER MAPA EM ARQUIVO .txt E PONTUACAO EM ARQUIVO .bin
-                break;
+        case 's':
+            printf("\r\t\t\t\t\tSALVAR JOGO\t\t\t\t\t");
+            //ESCREVER MAPA EM ARQUIVO .txt E PONTUACAO EM ARQUIVO .bin
+            break;
 
-            case 'r':
-                //printf("\r                       RETURNING GAME\t\t\t\t\t\t\t");
-                apaga_linha();
-                break;
+        case 'r':
+            //printf("\r                       RETURNING GAME\t\t\t\t\t\t\t");
+            apaga_linha();
+            break;
 
-            case 'q':
-                textcolor(RED);
-                printf("\r                        ENDING GAME\t\t\t\t\t\t\t");
-                textcolor(WHITE);
-                endgame = 1;
-                break;
+        case 'q':
+            textcolor(RED);
+            printf("\r                        ENDING GAME\t\t\t\t\t\t\t");
+            textcolor(WHITE);
+            endgame = 1;
+            break;
 
-            default:
-                goto start;
+        default:
+            goto start;
         }
-        if(!endgame){
+        if(!endgame)
+        {
             gotoxy(jogador->x, jogador->y);
         }
     }
@@ -148,107 +161,152 @@ int menu(int inicio, tipo_mapa *mapa, tipo_pos *jogador){//RETORNA 1 SE JOGO DEV
     return endgame;
 }
 
-void apaga_linha(){
+void apaga_linha()
+{
     printf("\r\t\t\t\t\t\t\t\t");
 }
 
-char teclado(){
+char teclado()
+{
     char c;
-    if(kbhit()){
+    if(kbhit())
+    {
         c = getch();
     }
     //printf("%c", c);
     return c;
 }
 
-void inicializa(char nome_arquivo[], tipo_mapa *mapa){
+void inicializa(char nome_arquivo[], tipo_mapa *mapa)
+{
     int i=0;
-    FILE *arquivo_mapa;//*mapa;
+    FILE *arquivo_mapa;
     char texto[62];
     arquivo_mapa = fopen(nome_arquivo, "r");
-    //mapa=fopen("mapa.txt","w+");
-    if(arquivo_mapa == NULL){
+    if(arquivo_mapa == NULL)
+    {
         printf("ERRO AO ABRIR MAPA!\n");
 
-    }else{
-        while(!feof(arquivo_mapa)){
+    }
+    else
+    {
+        while(!feof(arquivo_mapa))
+        {
             fgets(texto, 62, arquivo_mapa);
             strcpy(mapa->tamanho[i], texto);
             i++;
         }
     }
 
-    //fputs(texto,mapa);
     fclose(arquivo_mapa);
-    //fclose(mapa);
     mapa->largura = strlen(mapa->tamanho[0]) - 1;
     mapa->altura = i;
     //printf("Largura: %d\tAltura: %d", mapa->largura, mapa->altura);
 }
 
-void imprime(tipo_mapa mapa){
+void imprime(tipo_mapa mapa)
+{
     int i;
-    /*
-    FILE *mapa;
-    char texto[100];
-    mapa=fopen("mapa.txt","r+");
-    while (fgets(texto,100,mapa)!=NULL)
-        printf("%s",texto);
-    fclose(mapa);
-    */
-    for(i=0; i<mapa.altura; i++){
+    for(i=0; i<mapa.altura; i++)
+    {
         printf("%s", mapa.tamanho[i]);
 
     }
 
 }
 
-tipo_pos encontra(char personagem, tipo_mapa mapa){
-    int i, j;
+tipo_pos encontra(char personagem, tipo_mapa mapa)
+{
+    int i=0, j=0;
     tipo_pos posicao;
-    for(i=0; i<mapa.altura; i++){
-        for(j=0; j<mapa.largura; j++){
-            if(mapa.tamanho[i][j] == personagem){
-                posicao.x = j+1;//+1 em ambos para poder mapaizar na tela
+    //for(i=0; i<mapa.altura; i++){
+    //for(j=0; j<mapa.largura; j++){
+    do
+    {
+        do
+        {
+            if(mapa.tamanho[i][j] == personagem)
+            {
+                posicao.x = j+1;//+1 em ambos para ficar conforme as coordenadas da tela, que começa em 1,1
                 posicao.y = i+1;
                 //printf("\t%d %d\n", posicao.x, posicao.y);
                 break;
             }
-            //j++;
-        }//while(j<mapa.largura);
-        //j=0;
-        //i++;
-    }//while(i<mapa.altura);
+            j++;
+        }
+        while(j<mapa.largura);
+        j=0;
+        i++;
+    }
+    while(i<mapa.altura);
 
     return posicao;
 }
 
-void movimenta(tipo_pos *jogador, int direcao, char c, tipo_mapa mapa){
+void movimenta(tipo_pos *jogador, int direcao, char c, tipo_mapa* mapa)
+{
     //fflush(stdin); //gotoxy(0,0);
-    switch (direcao){
-        case 119://W
-        case 72: //PARA CIMA
+    int teste;
+    switch (direcao)
+    {
+    case 119://W
+    case 72: //PARA CIMA
+        if(mapa->tamanho[jogador->y-2][jogador->x-1]==' ')   //Comparo com o mapa, mas tiro um de cada para passar da coordenada 1,1 para 0,0. A subtração adicional é para ir para a proxima posição solicitadoa
+        {
             putchxy(jogador->x,jogador->y,' ');
-            putchxy(jogador->x,jogador->y-1,'&');
+            putchxy(jogador->x,jogador->y-1,c);
+            atualizamapa(c,mapa,jogador,'C'); //Dentro dessa função nao precisou do & antes de mapa e jogar pois nesse escopo eles já são ponteiros
             jogador->y--;
-            break;
-        case 115://S
-        case 80: //PARA BAIXO
+
+        }
+        break;
+    case 115://S
+    case 80: //PARA BAIXO
+        if(mapa->tamanho[jogador->y][jogador->x-1]==' ')
+        {
             putchxy(jogador->x,jogador->y,' ');
-            putchxy(jogador->x,jogador->y+1,'&');
+            putchxy(jogador->x,jogador->y+1,c);
+            atualizamapa(c,mapa,jogador,'B');
             jogador->y++;
-            break;
-        case 97: //A
-        case 75: //PARA ESQUERDA
+
+        }
+        break;
+    case 97: //A
+    case 75: //PARA ESQUERDA
+        if(mapa->tamanho[jogador->y-1][jogador->x-2]==' ')
+        {
             putchxy(jogador->x,jogador->y,' ');
-            putchxy(jogador->x-1,jogador->y,'&');
+            putchxy(jogador->x-1,jogador->y,c);
+            atualizamapa(c,mapa,jogador,'E');
             jogador->x--;
-            break;
-        case 100://D
-        case 77: //PARA DIREITA
+
+        }
+        break;
+    case 100://D
+    case 77: //PARA DIREITA
+        if(mapa->tamanho[jogador->y-1][jogador->x]==' ')
+        {
             putchxy(jogador->x,jogador->y,' ');
-            putchxy(jogador->x+1,jogador->y,'&');
+            putchxy(jogador->x+1,jogador->y,c);
+            atualizamapa(c,mapa,jogador,'D');
             jogador->x++;
-            break;
+        }
+        break;
+    }
+}
+
+void atualizamapa(char c,tipo_mapa* mapa,tipo_pos* jogador,char sentido)
+{
+    mapa->tamanho[jogador->y-1][jogador->x-1]=' ';
+    switch(sentido)
+    {
+    case 'c':
+        mapa->tamanho[jogador->y-2][jogador->x-1]=c;
+    case 'b':
+        mapa->tamanho[jogador->y][jogador->x-1]=c;
+    case 'e':
+        mapa->tamanho[jogador->y-1][jogador->x-2]=c;
+    case 'd':
+        mapa->tamanho[jogador->y-1][jogador->x]=c;
     }
 }
