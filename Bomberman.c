e924171143766e21c039fd6cb21a8624fba8bfd9
LucasEsteves4589 <51028280+LucasEsteves4589@users.noreply.github.com>
2019-06-02 16:10:30 -0300

    Save e load de saves

--- Bomberman.c
+++ Bomberman.c
@@ -24,14 +24,14 @@ void movimenta(tipo_pos *jogador, int direcao, char c, tipo_mapa* mapa);
 char teclado();
 int menu(int inicio, tipo_mapa *mapa, tipo_pos *jogador);
 void apaga_linha();
-void atualizamapa(char c,tipo_mapa* mapa,tipo_pos* jogador,char sentido);
-
+void atualizamapa(char c, tipo_mapa *mapa, tipo_pos *jogador, char sentido);
+void aguarda_teclado();
 
 int main()
 {
     int direcao, endgame = 0;
     tipo_pos jogador;
-    char nome_arquivo[30] = "mapabeta.txt", c = '&';
+    char nome_arquivo[30] = "mapa_fase1.txt", c = 'j';
     tipo_mapa mapa;
     textcolor(WHITE);
     /*printf("Entre o nome do arquivo que deseja abrir(sem o .txt no final):");
@@ -63,13 +63,15 @@ int main()
 
         }
     }
+    system("pause");
     return 0;
 }
 
 int menu(int inicio, tipo_mapa *mapa, tipo_pos *jogador) //RETORNA 1 SE JOGO DEVE ACABAR
 {
-    char c;
-    int endgame = 0;
+    FILE *save;
+    char c, save_num, nome_save[20] = "save_0.txt", lista_saves[9][20];
+    int num_saves=0, i, endgame = 0;
     if(inicio)
     {
         textcolor(LIGHTRED);
@@ -99,7 +101,7 @@ int menu(int inicio, tipo_mapa *mapa, tipo_pos *jogador) //RETORNA 1 SE JOGO DEV
         printf("\n#                  Press any key to continue               #");
         printf("\n############################################################");
         textcolor(WHITE);
-        while(!kbhit()) {}
+        aguarda_teclado();
         clrscr();
 
     }
@@ -111,54 +113,117 @@ start:
         printf("(N)New Game  (L)Load Game  (S)Save  (Q)Quit Game   (R)Return");
         textcolor(WHITE);
 
-        while(!kbhit()) {}
-        c = teclado();
-        switch(c)
-        {
-        case 'n':
-            clrscr();
-            inicializa("mapabeta.txt", mapa);
-            imprime(*mapa);
-            *jogador = encontra('&', *mapa);
-            //printf("\r\t\t\t\t\tNOVO JOGO\t\t\t\t\t");
-            break;
+        aguarda_teclado();
+        //c = teclado();
+        switch(teclado()){
+            case 'N':
+            case 'n':
+                clrscr();
+                inicializa("mapa_fase1.txt", mapa);
+                imprime(*mapa);
+                *jogador = encontra('j', *mapa);
+                break;
 
-        case 'l':
-            //printf("\r\t\t\t\t\tCARREGAR JOGO\t\t\t\t\t");
-            //CHAMAR LEITURA DE MAPA JA SALVO
-            clrscr();
-            inicializa("mapajogo.txt", mapa);
-            imprime(*mapa);
-            *jogador = encontra('&', *mapa);
-            break;
+            case 'L':
+            case 'l':
+                //CHAMAR LEITURA DE MAPA JA SALVO
+                i=0;
+                do{ //TESTA QUANTOS SAVES JA EXISTEM
+                    i++;
+                    save_num = i + '0';
+                    nome_save[5] = save_num;
+                    save = fopen(nome_save, "r");
+                    if(save != NULL){
+                        strcpy(lista_saves[i-1], nome_save); //COPIA O NOME DO SAVE PARA UMA STRING
+                        num_saves++;
+                        fclose(save);
+                    }
+                }while(save != NULL);
 
-        case 's':
-            printf("\r\t\t\t\t\tSALVAR JOGO\t\t\t\t\t");
-            //ESCREVER MAPA EM ARQUIVO .txt E PONTUACAO EM ARQUIVO .bin
-            break;
+                if(num_saves > 0){
+                    textcolor(LIGHTCYAN);
+                    for(i=0; i<num_saves; i++){ //IMPRIME A LISTA DE SAVES A DIRETIA DA TELA DO JOGO
+                        gotoxy(62, 3*i+1);
+                        printf("(%d)%s", i+1, lista_saves[i]);
+                    }
+                    textcolor(WHITE);
 
-        case 'r':
-            //printf("\r                       RETURNING GAME\t\t\t\t\t\t\t");
-            apaga_linha();
-            break;
+                    load:
+                    aguarda_teclado();
+                    save_num = teclado();
 
-        case 'q':
-            textcolor(RED);
-            printf("\r                        ENDING GAME\t\t\t\t\t\t\t");
-            textcolor(WHITE);
-            endgame = 1;
-            break;
+                    if(save_num<'1' || (save_num - '0')>num_saves){ //TESTA SE O USUARIO ESCOLHEU UM SAVE VALIDO, ENTRE 1 E NUM_SAVES
+                        goto load;
+                    }
 
-        default:
-            goto start;
+                    nome_save[5] = save_num;
+                    clrscr();
+
+                    inicializa(nome_save, mapa);
+                    imprime(*mapa);
+                    *jogador = encontra('j', *mapa);
+
+                }else{
+                    gotoxy(1,26);
+                    textcolor(LIGHTGREEN);
+                    printf("\r                       NO SAVED GAMES\t\t\t\t");
+                    textcolor(WHITE);
+                }
+
+                break;
+
+            case 'S':
+            case 's':
+                //ESCREVER PONTUACAO EM ARQUIVO .bin
+                i=0;
+                do{ //TESTA QUANTOS SAVES JA EXISTEM
+                    i++;
+                    save_num = i + '0';
+                    nome_save[5] = save_num;
+                    save = fopen(nome_save, "r");
+                    if(save != NULL){
+                        fclose(save);
+                    }
+                }while(save != NULL);
+
+                save = fopen(nome_save, "w");
+
+                for(i=0; i<mapa->altura; i++){
+                    fputs(mapa->tamanho[i], save);
+
+                }
+                fclose(save);
+                textcolor(LIGHTGREEN);
+                printf("\r                  GAME SAVED SUCCESSFULLY\t\t\t\t\t");
+                textcolor(WHITE);
+                break;
+
+            case 'R':
+            case 'r':
+                apaga_linha();
+                break;
+
+            case 'Q':
+            case 'q':
+                textcolor(LIGHTRED);
+                printf("\r                        ENDING GAME\t\t\t\t\t\t\n");
+                textcolor(WHITE);
+                endgame = 1;
+                break;
+
+            default:
+                goto start;
         }
-        if(!endgame)
-        {
+        if(!endgame){
             gotoxy(jogador->x, jogador->y);
         }
     }
 
     return endgame;
+}
+
+void aguarda_teclado(){
+    while(!kbhit()){}
 }
 
 void apaga_linha()
@@ -206,10 +271,33 @@ void inicializa(char nome_arquivo[], tipo_mapa *mapa)
 
 void imprime(tipo_mapa mapa)
 {
-    int i;
-    for(i=0; i<mapa.altura; i++)
-    {
-        printf("%s", mapa.tamanho[i]);
+    int i, j;
+    for(i=0; i<mapa.altura; i++){
+        for(j=0; j<mapa.largura; j++){
+            switch(mapa.tamanho[i][j]){
+                case 'w':
+                    printf("#");
+                    break;
+
+                case 'd':
+                    printf("&");
+                    break;
+
+                case 'k':
+                case 'b':
+                    printf("k");
+                    break;
+
+                case 'e':
+                    printf("i");
+                    break;
+
+                default:
+                    printf("%c", mapa.tamanho[i][j]);
+            }
+        }
+        printf("\n");
+        //printf("%s", mapa.tamanho[i]);
 
     }
 
@@ -254,8 +342,8 @@ void movimenta(tipo_pos *jogador, int direcao, char c, tipo_mapa* mapa)
         if(mapa->tamanho[jogador->y-2][jogador->x-1]==' ')   //Comparo com o mapa, mas tiro um de cada para passar da coordenada 1,1 para 0,0. A subtração adicional é para ir para a proxima posição solicitadoa
         {
             putchxy(jogador->x,jogador->y,' ');
-            putchxy(jogador->x,jogador->y-1,c);
-            atualizamapa(c,mapa,jogador,'C'); //Dentro dessa função nao precisou do & antes de mapa e jogar pois nesse escopo eles já são ponteiros
+            putchxy(jogador->x,jogador->y-1, c);
+            atualizamapa(c, mapa, jogador, 'c'); //Dentro dessa função nao precisou do & antes de mapa e jogar pois nesse escopo eles já são ponteiros
             jogador->y--;
 
         }
@@ -266,7 +354,7 @@ void movimenta(tipo_pos *jogador, int direcao, char c, tipo_mapa* mapa)
         {
             putchxy(jogador->x,jogador->y,' ');
             putchxy(jogador->x,jogador->y+1,c);
-            atualizamapa(c,mapa,jogador,'B');
+            atualizamapa(c, mapa, jogador, 'b');
             jogador->y++;
 
         }
@@ -277,7 +365,7 @@ void movimenta(tipo_pos *jogador, int direcao, char c, tipo_mapa* mapa)
         {
             putchxy(jogador->x,jogador->y,' ');
             putchxy(jogador->x-1,jogador->y,c);
-            atualizamapa(c,mapa,jogador,'E');
+            atualizamapa(c, mapa, jogador, 'e');
             jogador->x--;
 
         }
@@ -288,25 +376,32 @@ void movimenta(tipo_pos *jogador, int direcao, char c, tipo_mapa* mapa)
         {
             putchxy(jogador->x,jogador->y,' ');
             putchxy(jogador->x+1,jogador->y,c);
-            atualizamapa(c,mapa,jogador,'D');
+            atualizamapa(c, mapa, jogador, 'd');
             jogador->x++;
         }
         break;
     }
 }
 
-void atualizamapa(char c,tipo_mapa* mapa,tipo_pos* jogador,char sentido)
+void atualizamapa(char c, tipo_mapa *mapa, tipo_pos *jogador, char sentido)
 {
     mapa->tamanho[jogador->y-1][jogador->x-1]=' ';
     switch(sentido)
     {
     case 'c':
         mapa->tamanho[jogador->y-2][jogador->x-1]=c;
+        break;
+
     case 'b':
         mapa->tamanho[jogador->y][jogador->x-1]=c;
+        break;
+
     case 'e':
         mapa->tamanho[jogador->y-1][jogador->x-2]=c;
+        break;
+
     case 'd':
         mapa->tamanho[jogador->y-1][jogador->x]=c;
+        break;
     }
 }
